e0263458b7b88d29984c72b920944d66ffe7077c
SimayBozaci <simaybozaci@gmail.com>
2024-11-29 16:39:32 +0300

    İlk commit

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -71,7 +71,7 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     color: 'white',
-    marginBottom: 10,
+    marginBottom: 11,
   },
   input: {
     width: '90%',
